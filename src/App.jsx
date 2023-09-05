b54225d70cfe94ac24e27b4ed5a6f46ce00e1728
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-09-05 01:09:08 -0500

    configuraciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
-import './App.css'
-import Calculator from './components/Calculator';
-
-
+import Calculator from "./components/Calculator";
+import './index.css';
 function App() {
-
-
   return (
-   <Calculator/>
-  )
+    <>
+      <h1 className="font-mi-fuente-personalizada text-4xl">Calcupipo</h1>
+      <Calculator />
+      <div className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">Fondo rojo con texto blanco</div>
+    </>
+  );
 }
 
 export default App;
